beb3799129922bd568ef09f2537fc6919f603d62
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2016-05-13 11:12:41 -0300

    [FIX] Login filename when HSYNC or HSYNS range is out

--- source/Protocol.cpp
+++ source/Protocol.cpp
@@ -1373,7 +1373,8 @@ void Protocol::Process(const char *path, int len, mongo::DBClientConnection *dbC
 						}
 						catch (const std::out_of_range& e)
 						{
-							Error(TAG "Error %s. HSYNC sbt4.substr(%d, %d)", e.what(), inicio, lHsync);
+
+							Error(TAG "%s/%s/%s Error %s. HSYNC sbt4.substr(%d, %d)", clientName.c_str(), plate.c_str(), fileName.c_str(), e.what(), inicio, lHsync);
 						}
 
 						i += 5;
@@ -1412,7 +1413,7 @@ void Protocol::Process(const char *path, int len, mongo::DBClientConnection *dbC
 						}
 						catch (const std::out_of_range& e)
 						{
-							Error(TAG "Error %s. HSYNS sbt4.substr(%d, %d)", e.what(), inicio, lHsyns);
+							Error(TAG "%s/%s/%s Error %s. HSYNS sbt4.substr(%d, %d)", clientName.c_str(), plate.c_str(), fileName.c_str(), e.what(), inicio, lHsyns);
 						}
 
 						i += 5;
